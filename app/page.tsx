1dde8b661cae3a004a648cfe902f4dae25f2a445
rcsang <sangmath@outlook.com>
2024-09-24 21:47:37 +0200

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-8 leading-tight tracking-tighter">welcome to solanasign</h1>
+      <h1 className="text-4xl font-bold mb-8 leading-tight tracking-tighter">
+        welcome to{" "}
+        <span className="bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 text-transparent bg-clip-text">
+          solana sign
+        </span>
+      </h1>
       <div className="flex gap-4 justify-between">
-      <Button>
-        <Link href="/sign-document">
-          Sign a Document
-        </Link>
-      </Button>
-      <Button>
-        <Link href="/upload-document">
-          Upload Document
-        </Link>
-      </Button>
+        <Button>
+          <Link href="/sign-document">Sign a Document</Link>
+        </Button>
+        <Button>
+          <Link href="/upload-document">Upload Document</Link>
+        </Button>
       </div>
     </div>
   );
