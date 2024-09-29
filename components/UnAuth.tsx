d92aea1b70b9b457fd723e2781d4d050354ec42d
rcsang <sangmath@outlook.com>
2024-09-29 14:14:21 +0200

    commit

--- components/UnAuth.tsx
+++ components/UnAuth.tsx
@@ -1,4 +1,3 @@
-import { WalletConnectButton } from "@solana/wallet-adapter-react-ui";
 import { Hero } from "./Hero";
 
 export const UnAuth = () => {
@@ -8,7 +7,6 @@ export const UnAuth = () => {
       <span className="mb-8 leading-tight tracking-tighter">
         connect your wallet to get started
       </span>
-      <WalletConnectButton />
     </div>
   );
 };
